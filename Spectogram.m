f1c86e20b1bbc1dee4e89ac554e295355643e589
Expez <mrgrandmother@gmail.com>
2011-10-15 15:29:51 +0200

    Attempted to make a Hamming window. Get no ouput.

--- Spectogram.m
+++ Spectogram.m
@@ -13,12 +13,19 @@ and make a spectrogram*)
 (*Number of data points in each slice. A resolution of 100 slices per second.*)
 sliceSize = sampleRate / 100;
 
+(*Apply a Hammond window to each segment*)
+hammond[x_] := Table[(0.54+0.46*Cos(2*Pi*i/(Length[x]))) * x[[i]], {i, 1, Length[x]}]
+hammondSlices = Table[hammond[Data[[i;;i+sliceSize-1]]], {i, 1,
+ Length[data]-sliceSize, sliceSize}];
+slices = Flatten[hammondSlices];
+
+(*Apply fourier transform to each window*)
 slices = Table[Fourier[data[[t;; t+sliceSize-1]] ], {t, 1, 
 Length[data] - sliceSize, sliceSize}];
 
 slices = Abs[slices];
 slices = Function[x, x/2N[Pi,6]]/@slices;
-slices;
+slices
 ListContourPlot[slices]
 ]
 spectrogram["c:/trainingData/go_0.wav"]
