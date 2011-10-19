494a04b65d49d03bc08ac6992987d777073b90c1
Tormund Haus <tormundhaus@gmail.com>
2011-10-19 16:22:54 +0200

    Finished features.m, moved sound files

--- features.m
+++ features.m
@@ -1,8 +1,9 @@
 (* ::Package:: *)
 
-
 (* Returns the featuresof the file found at path *)
-features[path] := Module[{sampleRate, data, frameSize, overlap},
+extract[path_] := Module[{sampleRate, data, frameSize, overlap},
+
+Print[path];
 
 (*Import the data, grab the sampling rate*)
 sampleRate = Import[path, "SampleRate"];
@@ -23,20 +24,18 @@ frames = Table[data[[i;;i+frameSize-1]],{i,1,Length[data]-frameSize,frameSize-ov
 hamming[x_] := Table[(0.54+0.46*Cos[2*\[Pi]*i/(Length[x])]) * x[[i]], {i, 1, Length[x]}];
 frames = Map[hamming,frames];
 (*Apply fourier transform to each window*)
-frames = Map[Fourier,frames];
-
+frames = Fourier /@ frames;
 frames = Abs[frames];
-frames = Function[x, x/2N[Pi,6]]/@frames;
 frames = Map[DeleteDuplicates, frames];
-findPeaks[list, peaks] := Sort[list, Greater][[1;;peaks]];
-
-features = Table[findPeaks[frames[[i]], 6], {i, 1, Length[frames]}]
-
-]
-path = "d:\\go_0.wav"
-features = features[path]
+Return[Ordering[#,6,Greater] & /@ frames];
+];
+path = "git/shoebox/training/go/go_0.wav";
+features = extract[Directory[] <> "/" <> path];
+Print[features];
 
 
 
 
 
+(* ::InheritFromParent:: *)
+(**)
