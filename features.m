95f8faec249c2c7062628a4731c73d422a6007a9
Tormund Haus <tormundhaus@gmail.com>
2011-10-22 00:23:37 +0200

    Refactored extract[]

--- features.m
+++ features.m
@@ -1,40 +1,31 @@
 (* ::Package:: *)
 
-(* Returns the featuresof the file found at path *)
-extract[path_] := Module[{sampleRate, data, frameSize, overlap,peaks},
+(* Returns the features of the file found at path *)
+extract[path_] := Module[{sampleRate, data, frameSize,overlap},
+	(*Import the data, grab the sampling rate*)
+	sampleRate = Import[path, "SampleRate"];
+	data = Flatten@Import[path,"Data"];
 
-Print[path];
+	(*Number of data points in each slice. A resolution of 100 slices per second. With a sample rate of 8000Hz*)
+	frameSize = 80;
+	overlap = 20;
 
-(*Import the data, grab the sampling rate*)
-sampleRate = Import[path, "SampleRate"];
-data = Flatten@Import[path,"Data"];
+	(*Divide soundfile into slices. Perform fourier transform on the slices, and make a spectrogram*)
+	frames = Table[data[[i;;i+frameSize-1]],{i,1,Length[data]-frameSize,frameSize-overlap}];
 
-(*Divide soundfile into slices. Perform fourier transform on the slices,
-and make a spectrogram*)
+	(*Apply a Hamming window to each frame*)
+	hamming[frame_] := Table[(0.54+0.46*Cos[2*\[Pi]*i/(Length[frame])]) * frame[[i]], {i, 1, Length[frame]}];
+	frames = hamming /@ frames;
 
-(*Number of data points in each slice. A resolution of 100 slices per second.*)
-frameSize = 80;
-overlap = 20;
-
-(*Apply a Hamming window to each segment*)
-
-frames = Table[data[[i;;i+frameSize-1]],{i,1,Length[data]-frameSize,frameSize-overlap}];
-
-
-hamming[x_] := Table[(0.54+0.46*Cos[2*\[Pi]*i/(Length[x])]) * x[[i]], {i, 1, Length[x]}];
-frames = Map[hamming,frames];
-(*Apply fourier transform to each window*)
-frames = Fourier /@ frames;
-frames = Abs[frames];
-frames = Map[DeleteDuplicates, frames];
-Print[ListLinePlot[frames[[20]]]];
-frames = Take[Sort[Flatten[Position[Partition[Differences[#], 2, 1], {x_, y_} /; Sign[x] > Sign[y]] + 1],Function[{e1,e2},#[[e1]] > #[[e2]]]],3] & /@ frames;
-Return[frames];
+	(*Apply fourier transform to each window*)
+	frames = Fourier /@ frames;
+	(* Get theabsolute value *)
+	frames = Abs[frames];
+	frames = Map[DeleteDuplicates, frames];
+	(*Find all peaks, sort them by amplitude, desc, and take the 3 first frequencies*)
+	frames = Take[Sort[Flatten[Position[Partition[Differences[#], 2, 1], {x_, y_} /; Sign[x] > Sign[y]] + 1],Function[{e1,e2},#[[e1]] > #[[e2]]]],3] & /@ frames;
+	Return[frames];
 ];
-path = "git/shoebox/training/go/go_0.wav";
-features = extract[Directory[] <> "/" <> path];
-Print[features];
-
 
 
 
