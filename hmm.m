f9ef67af66780241bec5c1923d23b8d6bd650538
Liei <tormunds@stud.ntnu.no>
2011-10-25 21:54:10 +0200

    fuck it

--- hmm.m
+++ hmm.m
@@ -4,7 +4,7 @@ normalize[lst_] := lst/Total[lst];
 
 forward[f_,A_,Bs_] := FoldList[#1.Transpose[A].#2 &,f,Bs];
 
-scaledForward[f_,A_,Bs_] := Transpose[FoldList[{normalize[#],Total[#]}&[#2.Transpose[A].#1[[1]]] &,{Bs[[1]].f,Total[Bs[[1]].f]},Bs[[2;;-1]]]];
+scaledForward[f_,A_,Bs_] := Transpose[FoldList[{#,Total[#]}&[#2.Transpose[A].#1[[1]]] &,{Bs[[1]].f,Total[Bs[[1]].f]},Bs[[2;;-2]]]];
 
 backward[b_,A_,Bs_] := Reverse[FoldList[A.#2.#1 &,b,Reverse[Bs]]];
 
@@ -31,10 +31,6 @@ scaledForwardBackward[fInit_,A_,Bs_] := Module[{f,l,b,likelihood},
 
 
 (* ::Input:: *)
-(*A  = {{0.7,0.3},{0.3,0.7}};*)
-(*U  = {{0.9,0.0},{0.0,0.2}};*)
-(*NU = {{0.1,0.0},{0.0,0.8}};*)
-(*Bs = {U,U,NU,U,U};*)
 (**)
 
 
@@ -42,7 +38,7 @@ findCalcB[mu_, sigma_] := Function[frame,DiagonalMatrix[Table[PDF[MultinormalDis
 
 newModel[{A_,mu_,sigma_}] := Module[{calcB},
 	calcB = findCalcB[mu,sigma];
-	Return[Function[frames,(forward[Table[0.5,Length[A]],A,calcB /@ frames])[[2]]]];
+	Return[Function[frames,(likelihood[Table[0.5,Length[A]],A,calcB /@ frames])[[2]]]];
 ];
 
 addModel[classifier_,word_,model_] := Append[classifier,{word,model}];
@@ -62,21 +58,23 @@ emInit[Os_, N_] := Module[{A,prior, mu, sigma,D,mean},
 	Return[{prior,A,mu,sigma,Os}];
 ];
 
-emStep[{prior_,A_,mu_,sigma_,Os_}] := Module[{T,S,F,r,f,t,muj,sum,\[Gamma],\[Xi],likelihood,gamma,xi,Aden,Anum,i,j,t,k,l,calcB,newPi,newA,newMu,newSigma},
+emStep[{prior_,A_,mu_,sigma_,Os_}] := Module[{T,S,F,r,f,muj,sum,\[Gamma],\[Xi],likelihood,gamma,xi,Aden,Anum,i,j,t,k,l,calcB,newPi,newA,newMu,newSigma},
 	(* Do step *) 
 	T = Length[Os];
 	S = Length[A];
 	F = Length[Os[[1]]];
 	
 	calcB = findCalcB[mu,sigma];
+	
 	{f,r} = scaledForwardBackward[prior,A,calcB /@ Os];
+
 	\[Gamma] = normalize[Times @@ #] & /@ Transpose[{f,r}];
 
 	newMu = Table[
 		Total[Table[(\[Gamma][[t,j]] * Os[[t]]),{t,T}]] / Total[Table[\[Gamma][[t,j]],{t,T}]],
 		{j,S}
 	];
-
+	
 	newSigma = Table[
 		Total[Table[\[Gamma][[t,j]] * Transpose[{Os[[t]] - newMu[[j]]}].{Os[[t]] - newMu[[j]]},{t,T}]] / Total[Table[\[Gamma][[t,j]],{t,T}]],
 		{j,S}
@@ -85,21 +83,21 @@ emStep[{prior_,A_,mu_,sigma_,Os_}] := Module[{T,S,F,r,f,t,muj,sum,\[Gamma],\[Xi]
 	newPi = \[Gamma][[1]];
 	
 	\[Xi] = Table[
-		sum = Total[Table[f[[k]] * A[[k,l]] * calcB[[Os[[t]]][[l,l]]] * r[[t,l]] ,{l,S},{k,S}]];
+		sum = Total[Table[f[[t,k]] * A[[k,l]] * calcB[Os[[t+1]]][[l,l]] * r[[t,l]] ,{k,S},{l,S}]];
+		Print[sum];
 		Table[	
-			(f[[t,i]] * A[[i,j]] * calcB[Os[[t]]][[j,j]] * r[[t+1,j]]) / sum
-			{i,1,Length[A]},
-			{j,1,Length[A]}		
+			(f[[t,i]] * A[[i,j]] * calcB[Os[[t+1]]][[j,j]] * r[[t+1,j]]) / sum,
+			{i,S},
+			{j,S}		
 		],
-		{t,1,Length[Os] - 1}
+		{t,T - 1}
 	];
-
+	
 	newA = Table[
 		Total[Table[\[Xi][[t,i,j]],{t,T}]] / Total[Table[\[Gamma][[t,j]],{t,T}]];
 		{i,S},
 		{j,S}
 	];
-
 	Return[{newPi,newA,newMu,newSigma,Os}];
 ];
 
@@ -120,27 +118,3 @@ baumWelch[frames_,N_] := Module[{pi,A,mu,sigma,diff,lastLikelihood,test},
 
 (* ::Input:: *)
 (*-*)
-
-
-(* ::Input:: *)
-(*Os = {{3,2},{3,4},{2,3}}*)
-
-
-(* ::Input:: *)
-(*{{3,2},{3,4,5},{2,3,4}}*)
-
-
-(* ::Input:: *)
-(*emInit[Os,3]*)
-
-
-(* ::Input:: *)
-(*emStep[%]*)
-
-
-(* ::InheritFromParent:: *)
-(**)
-
-
-(* ::Input:: *)
-(*Transpose[{{a,b}}].{{x,y}}*)
